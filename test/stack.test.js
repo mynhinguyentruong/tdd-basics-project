6d0231fc8f40d46c05d9cc1310a5dd1689ebbdb3
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-09-22 10:13:40 -0400

    Update stack.test.js

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,1 +1,19 @@
-a
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {}
+  }
+}
+
+describe("My stack", () => {
+  it("is created empty", () => {
+    const stack = new Stack();
+
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toStrictEqual({})
+  });
+
+  it.todo("can push item to the top");
+
+  it.todo("can pop off");
+});
